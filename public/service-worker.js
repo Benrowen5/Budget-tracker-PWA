2fb347f67c1d9b1cdac4010c7807064d7fa8279c
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-01-10 18:47:34 -0500

    updated serviceWorker.js and index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,11 +41,11 @@ self.addEventListener('fetch', function (e) {
       caches.match(e.request).then(function (request) {
         // if cache is available, respond with cache
         if (request) { 
-          console.log('responding with cache : ' + e.request.url)
-          return request
+          console.log('responding with cache : ' + e.request.url);
+          return request;
         } else {
           // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
+          console.log('file is not cached, fetching : ' + e.request.url);
           return fetch(e.request);
         }
       })
