34f29e844ae61996909faa832d43f9307c7cd423
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2022-01-10 16:37:51 -0500

    updated Service Worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,7 +25,7 @@ self.addEventListener('activate', function(e) {
                 return key.indexOf(APP_PREFIX);
             })
             cacheKeeplist.push(CACHE_NAME);
-            return promises.all(keyList.map(function(key, i) {
+            return promise.all(keyList.map(function(key, i) {
                 if (cacheKeeplist.indexOf(key) === -1) {
                     console.log('Deleting cache : ' + keyList[i] );
                     return caches.delete(keyList[i]);
